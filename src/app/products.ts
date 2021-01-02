6ab1db5e5a938e25b070affae729d24a69bc3cf0
samarjeet-singh <mrinal8261@gmail.com>
2021-01-02 15:24:29 +0530

    3 files modified, 7 added

---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: "IPhone XL",
+    price: "50000",
+    description: "A phone from Apple with great features"
+  },
+  {
+    name: "Samsung Mobile",
+    price: "40000",
+    description: "A phone from Samsung With a Latest features"
+  },
+  {
+    name: "OnePlus",
+    price: "70000",
+    description: "A phone with a functionality with new Features"
+  },
+  {
+    name: "Indian Mobile",
+    price: "100000",
+    description: "A phone with Unique Features"
+  }
+];
