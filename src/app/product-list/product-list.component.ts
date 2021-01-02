abb03f8d5ab5dd0761cfeab2abf9eb44e7353c26
samarjeet-singh <mrinal8261@gmail.com>
2021-01-02 21:43:22 +0530

    4 files modified, 5 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from "@angular/core";
+import { products } from "../products";
 
 @Component({
   selector: "app-product-list",
   templateUrl: "./product-list.component.html",
   styleUrls: ["./product-list.component.css"]
 })
-export class ProductListComponent implements OnInit {
-  
-  constructor() {}
-
-  ngOnInit() {}
+export class ProductListComponent {
+  products = products;
+  Share() {
+    window.alert("Product has been shared");
+  }
+  onNotify() {
+    window.alert("you will be Notified when product price goes above 50000");
+  }
 }
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,20 +1,24 @@
 export const products = [
   {
+    id: "1",
     name: "IPhone XL",
     price: "50000",
     description: "A phone from Apple with great features"
   },
   {
+    id: "2",
     name: "Samsung Mobile",
     price: "40000",
     description: "A phone from Samsung With a Latest features"
   },
   {
+    id: "3",
     name: "OnePlus",
     price: "70000",
     description: "A phone with a functionality with new Features"
   },
   {
+    id: "4",
     name: "Indian Mobile",
     price: "100000",
     description: "A phone with Unique Features"
